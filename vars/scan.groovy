9a118947e3d0209b6189ad27c1f07bce0b1a7613
tamilzh2018 <tamizhnationalist@gmail.com>
2025-02-07 14:52:07 +0530

    Update scan.groovy

--- vars/scan.groovy
+++ vars/scan.groovy
@@ -1,5 +1,6 @@
-def call() {
-sh """
-mvn sonar:sonar
-"""
+def call(String sonarProjectKey) {
+    withSonarQubeEnv('SonarQube') {
+        sh "mvn clean verify sonar:sonar -Dsonar.projectKey=${sonarProjectKey} -Dsonar.host.url=${env.SONAR_HOST_URL} -Dsonar.login=${env.SONAR_TOKEN}"
+    }
 }
+
