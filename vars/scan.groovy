abe2d1fdd60ea92be53612e09f6aeee2dc87fb84
tamilzh2018 <tamizhnationalist@gmail.com>
2025-02-07 12:11:17 +0530

    sonar token added

--- vars/scan.groovy
+++ vars/scan.groovy
@@ -4,6 +4,7 @@ mvn clean deploy \
 sonar:sonar \
 -DbuildNumber=${BUILD_NUMBER}  \
     -Drelease-repository.url=http://10.0.0.130:8081/repository/maven-releases/  \
-    -Dsnapshot.repository.url=http://10.0.0.130:8081/repository/maven-snapshots/
+    -Dsnapshot.repository.url=http://10.0.0.130:8081/repository/maven-snapshots/ \
+    -Dsonar.token=squ_62694a7551bf44e025ceb3f4d99960724d657806
     '''
 }
