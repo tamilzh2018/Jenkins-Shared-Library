f80615a89c521dc79d5a319e3d6dfeed75fecd4d
tamilzh2018 <tamizhnationalist@gmail.com>
2025-02-07 17:49:31 +0530

    Update nexusDeploy.groovy

--- vars/nexusDeploy.groovy
+++ vars/nexusDeploy.groovy
@@ -1,9 +1,7 @@
 def call(String repositoryId, String credentialsId) {
-   
+    echo "nexusDeploy: repositoryId=${repositoryId}, credentialsId=${credentialsId}"
     withCredentials([usernamePassword(credentialsId: 'Nexus-Credentials', passwordVariable: 'Nexus-Password', usernameVariable: 'Nexus-Username'), string(credentialsId: credentialsId, variable: 'NEXUS_REPO_URL')]) {
+        echo "Deploying to Nexus repository: ${NEXUS_REPO_URL}"
         sh "mvn deploy -DskipTests -Dmaven.wagon.http.ssl.insecure=true -Dmaven.wagon.http.ssl.allowall=true -DaltDeploymentRepository=${repositoryId}::default::${NEXUS_REPO_URL} -Dusername=${NEXUS_USERNAME} -Dpassword=${NEXUS_PASSWORD}"
     }
 }
-
-
-
